35d1e365f4bbefcfdbd5125cf0c25ac06735efe8
JasmineHaley <47067968+JasmineHaley@users.noreply.github.com>
2020-11-15 10:28:06 -0400

    Add files via upload

--- assignment2_00/profile.php
+++ assignment2_00/profile.php
@@ -1,8 +1,8 @@
 <?php 
-
-require 'autoloader.php';
- require 'config.php'; 
-
-$controller = new ProfileController();
-$controller->run();
+//use Apps\handlers\IndexController;
+use Quwius\Framework\FrontController;
+require '/Applications/XAMPP/xamppfiles/400001020/assignment2_00/config.php'; 
+require ROOT_DIR.'/autoloader.php';
+ 
+FrontController :: run();
  ?>
